63148e5a172d273b5df8a246115d547c5e8a60c3
emailkolar <emilaalund.nyboder@gmail.com>
2025-03-26 09:28:52 +0100

    new sse example

---
+++ 01._Assignments/04._Server-To-Client/04a._SSE/01._Node/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/sse",(req,res)=>{
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', "keep-alive")
+
+    res.write(`data: connected to server \n\n`)
+    let counter = 0;
+    const intervalId = setInterval(() => {
+        counter++;
+        res.write(`data: Message ${counter}\n\n`);
+    }, 2000);
+
+    
+    req.on('close', () => {
+        clearInterval(intervalId);
+        res.end();
+    });
+
+});
+
+
+const PORT = (8000)
+app.listen(PORT, ()=>{
+    console.log(`app started on port: ${PORT}`)
+})
